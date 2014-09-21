e0364d186b0fbdb463def3c2de6d87983ef6b234
molst <marcus.holst@intelliplan.se>
2014-09-21 21:00:21 +0200

    open TCP 5555 for incoming events, link to riemann-burst

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box = 'centos-6.5-x64'
   config.vm.synced_folder '.', '/vagrant', mount_options: ["dmode=775","fmode=777"]
-  config.vm.network :forwarded_port, guest: 5557, host: 5557
+  config.vm.network :forwarded_port, guest: 5555, host: 5555 # riemann tcp
+  config.vm.network :forwarded_port, guest: 5557, host: 5557 # riemann repl
   config.vm.provision "shell", path: "provision.sh"
 end
 
--- riemann-config-user.clj
+++ riemann-config-user.clj
@@ -1,6 +1,8 @@
 (in-ns 'riemann.config)
 
-(streams #(info "received event " %)) ;;log all received events
+(streams (where (service "debug-metric-series")
+                (fn [{:keys [description metric]}] ;;log all debug-metric-series events
+                  (println description " now: " metric))))
 
 ;;riemann requires a specific call to pick up the newly loaded config changes
 ;;in emacs with CIDER, place cursor right after the right paren below and press C-x C-e
